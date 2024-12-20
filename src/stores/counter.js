79f73b3af7b6d164b667defbafa3c0a6df06ff60
Moazam05 <salmanmoazam08@gmail.com>
2024-12-20 15:14:57 +0500

    persist pinia

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -1,11 +1,10 @@
 import { defineStore } from "pinia";
 
-export const useCounterStore = defineStore({
-  id: "counter",
-
+export const useCounterStore = defineStore("counter", {
   state: () => ({
     count: 0,
   }),
+  persist: true,
 
   actions: {
     increaseCount() {
